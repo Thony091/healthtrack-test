6d3a7165ef1d8ac065bf186f8957cb9d03188b83
Thony091 <121407856+Thony091@users.noreply.github.com>
2025-06-29 21:53:56 -0400

    fix set weight method

--- src/main/java/com/healthtrack/Usuario.java
+++ src/main/java/com/healthtrack/Usuario.java
@@ -2,28 +2,28 @@ package com.healthtrack;
 
 public class Usuario {
 
-  private String nombre;
+  final private String nombre;
   private double peso;
 
   public Usuario(String nombre, double peso) {
-      this.nombre = nombre;
-      this.peso = peso;
+    this.nombre = nombre;
+    this.peso = peso;
   }
 
   public String getNombre() {
-      return nombre;
+    return nombre;
   }
 
   public double getPeso() {
-      return peso;
+    return peso;
   }
 
   public void actualizarPeso(double nuevoPeso) {
-      // ERROR: En lugar de asignar el nuevo peso, se está restando 1kg.
-      this.peso -= 1;
+    // LÓGICA CORREGIDA: Asignar el nuevo peso ingresado.
+    this.peso = nuevoPeso;
   }
 
   public void mostrarInformacion() {
-      System.out.println("Usuario: " + nombre + ", Peso Actual: " + peso + " kg");
+    System.out.println("Usuario: " + nombre + ", Peso Actual: " + peso + " kg");
   }
 }
